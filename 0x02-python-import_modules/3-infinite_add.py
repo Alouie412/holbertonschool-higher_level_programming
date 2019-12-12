3c26eabdf1d454ac438593365aeb46bf93a4d91f
Alouie412 <alouie412@gmail.com>
2019-12-12 22:06:51 +0000

    Pep8 style format

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-import sys
-num = len(sys.argv) - 1
-total = 0
-convertedInt = 0
 if __name__ == '__main__':
-	for i in range(1, num + 1):
-		convertedInt = int(sys.argv[i])
-		total = total + convertedInt
-	print("{}".format(total))
+    import sys
+    num = len(sys.argv) - 1
+    total = 0
+    convertedInt = 0
+    for i in range(1, num + 1):
+        convertedInt = int(sys.argv[i])
+        total = total + convertedInt
+    print("{}".format(total))
