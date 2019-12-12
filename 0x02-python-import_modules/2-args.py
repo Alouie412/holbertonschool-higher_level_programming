0356915750176bbdaa41e370ad2b85a79cececb1
Alouie412 <alouie412@gmail.com>
2019-12-12 22:13:16 +0000

    Fixed incorrect logic

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,13 +2,12 @@
 if __name__ == '__main__':
     import sys
     num = len(sys.argv) - 1
-    if __name__ == '__main__':
-        if num == 0:
-            print("0 arguments.")
-        elif num == 1:
-            print("1 argument:")
-            print("1: {}".format(sys.argv))
-        else:
-            print("{} arguments:".format(num))
-            for i in range(1, num + 1):
-                print("{}: {}".format(i, sys.argv[i]))
+    if num == 0:
+        print("{} arguments.".format(num))
+    elif num == 1:
+        print("{} argument:".format(num))
+        print("1: {}".format(sys.argv[1]))
+    else:
+        print("{} arguments:".format(num))
+        for i in range(1, num + 1):
+            print("{}: {}".format(i, sys.argv[i]))
