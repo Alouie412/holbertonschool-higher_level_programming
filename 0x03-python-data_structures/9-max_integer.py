1a4211b41e96186343af06766e63e8b829214b8b
Alouie412 <alouie412@gmail.com>
2019-12-18 06:54:45 +0000

    Modified logic: for loop

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -4,7 +4,7 @@ def max_integer(my_list=[]):
         return None
 
     max_num = 0
-    for i in range(0, len(my_list) - 1):
+    for i in range(0, len(my_list)):
         if my_list[i] > max_num:
             max_num = my_list[i]
 
