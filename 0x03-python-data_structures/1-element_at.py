effc3aa1c6903ef046ea1d39126b717d81be9628
Alouie412 <alouie412@gmail.com>
2019-12-18 06:04:17 +0000

    Fixed typo + logic fix

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,10 +2,7 @@
 def element_at(my_list, idx):
     if idx < 0:
         return None
-    else:
-        i = 0
-        for i in my_list:
-            if i == idx:
-                return my_list[i]
-
+    elif idx > len(my_list):
         return None
+    else:
+        return my_list[idx]
