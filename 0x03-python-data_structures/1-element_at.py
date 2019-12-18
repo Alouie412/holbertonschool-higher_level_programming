12091efeb501b6f0774e55a7e08efa1d06bfef5e
Alouie412 <alouie412@gmail.com>
2019-12-18 05:57:59 +0000

    Initial commit

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    if idx < 0:
+        return None
+    else:
+        i = 0
+        for i in my_list:
+            if i == idx:
+                return my_list[i]
+
+        return None
