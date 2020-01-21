abe2768363ebfdb386a07b7e0f1b191b14053219
Alouie412 <alouie412@gmail.com>
2020-01-21 10:08:25 +0000

    Comments should be exempted from the 80 char restriction

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def read_file(filename=""):
     """
-    This set of code would work for opening and reading a file, but the instructions
-    state that the with statement must be used
+    This set of code would work for opening and reading a file, but the
+    instructions state that the with statement must be used
     f = open('filename', 'r')
     f.read()
     f.close()
