6194f30f9b723f3c7aec609b4586f2d5b4efab98
Alouie412 <alouie412@gmail.com>
2020-01-23 15:21:55 +0000

    Added conversion from dictionary to json string. Also an edge case. Task 15

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -19,5 +19,5 @@ class Base():
     def to_json_string(list_dictionaries):
         """ Converting dictionaries to json strings """
         if list_dictionaries is None or len(list_dictionaries) is 0:
-            return ("[]")
+            return json.dumps()
         return json.dumps(list_dictionaries)
