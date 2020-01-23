2cb11ea95deda1122ee90e61ad002b994dd90dcd
Alouie412 <alouie412@gmail.com>
2020-01-23 14:43:24 +0000

    Added getter/setter function for size. Task 11

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -18,3 +18,18 @@ class Square(Rectangle):
         """ str() function, overloaded """
         return ("[{}] ({}) {}/{} - {}".format("Square", self.id,
                 self.x, self.y, self.width))
+
+    @property
+    def size(self):
+        """ Size getter function """
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """ Size setter function """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.width = value
+        self.height = self.width
