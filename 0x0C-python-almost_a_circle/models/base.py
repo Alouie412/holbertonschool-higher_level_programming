b12c662e3d29eeed768567b12c39d7d243e28e40
Alouie412 <alouie412@gmail.com>
2020-01-23 11:09:15 +0000

    Added documentation for each function

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
+""" Base.py """
 
 
 class Base():
+    """ Base class. Handles id of future classes """
     __nb_objects = 0
     def __init__(self, id=None):
+        """ Initialization function """
         if id is not None:
             self.id = id
         else:
--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,49 +1,70 @@
 #!/usr/bin/python3
+""" rectangle.py """
+""" File that handles anything related to the rectangle class """
+""" This file imports the code found in /models/base.py """
 from models.base import Base
 
 
 class Rectangle(Base):
+    """ Rectangle class. Imports the Base class """
     def __init__(self, width, height, x=0, y=0, id=None):
+        """ Initialization """
         super().__init__(id)
         Rectangle.set_width(self, width)
         Rectangle.set_height(self, height)
         Rectangle.set_x(self, x)
         Rectangle.set_y(self, y)
 
+    @property
     def get_width(self):
+        """ Width getter function """
         return self.__width
 
+    @width.setter
     def set_width(self, value):
+        """ Width setter function """
         if type(value) is not int:
             raise TypeError("width must be an integer")
         if value <= 0:
             raise ValueError("width must be > 0")
         self.__width = value
 
+    @property
     def get_height(self):
+        """ Height getter function """
         return self.__height
 
+    @height.setter
     def set_height(self, value):
+        """ Height setter function """
         if type(value) is not int:
             raise TypeError("height must be an integer")
         if value <= 0:
             raise ValueError("height must be > 0")
         self.__height = value
 
+    @property
     def get_x(self):
+        """ X getter function """
         return self.__x
 
+    @x.setter
     def set_x(self, value):
+        """ X setter function """
         if type(value) is not int:
             raise TypeError("x must be an integer")
         if value < 0:
             raise ValueError("x must be >= 0")
         self.__x = value
 
+    @property
     def get_y(self):
+        """ Y getter function """
         return self.__y
 
+    @y.setter
     def set_y(self, value):
+        """ Y setter function """
         if type(value) is not int:
             raise TypeError("y must be an integer")
         if value < 0:
@@ -51,19 +72,24 @@ class Rectangle(Base):
         self.__y = value
 
     def area(self):
+        """ Area function. Returns the area """
         return self.__width * self.__height
 
     def display(self):
+        """ Display function. Prints and shifts the shape with '#' """
         for i in range(0, self.__y):
             print("")
         for i in range(0, self.__height):
             print(" " * self.__x + "#" * self.__width)
 
     def __str__(self):
+        """ str() function, overloaded """
         return ("[{}] ({}) {}/{} - {}/{}".format("Rectangle",
                 self.id, self.__x, self.__y, self.__width, self.__height))
 
     def update(self, *args, **kwargs):
+        """ Update function. Allows for changing values """
+        """ Supports input in tuple (*arg) or dictionary (**kwarg) format """
         length = len(args)
         if length is not 0:
             print("In args")
