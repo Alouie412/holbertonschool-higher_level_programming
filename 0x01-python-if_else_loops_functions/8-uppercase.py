eddc812fc6578167b553f2311ed4559a98db22d9
Alouie412 <alouie412@gmail.com>
2020-02-01 12:21:56 +0000

    Fixed incorrect logic: wrong usage of print

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def uppercase(str):
+    uppercase = 0
     for char in str:
         if ord(char) >= ord('a') and ord(char) <= ord('z'):
-            print("{}".format(ord(char) - 32), end='')
+            uppercase = 32
         else:
-            print("{}".format(ord(char)), end='')
-        print()
+            uppercase = 0
+        print("{:c}".format(ord(char) - uppercase), end='')    
+    print()
