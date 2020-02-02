1b3a5878745a4d5df53e85dd02fe9069670e05ee
Alouie412 <alouie412@gmail.com>
2020-02-02 10:12:41 +0000

    Initial commit: Get highest value in dictionary

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    best_score = 0
+    best_name = ""
+
+    if a_dictionary is None:
+        return None
+
+    for key, value in a_dictionary.items():
+        if value > best_score:
+            best_score = value
+            best_name = key
+
+    return best_name
