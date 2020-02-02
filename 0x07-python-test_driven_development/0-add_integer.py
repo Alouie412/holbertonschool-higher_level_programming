0f139b86c8a08c64c6aa89e61652b21355a558fd
Alouie412 <alouie412@gmail.com>
2020-02-02 11:06:45 +0000

    Added bizarre NaN edge case

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def add_integer(a, b=98):
-    if type(a) is int or type(a) is float:
+    if type(a) is int or type(a) is float or a is NaN:
         a = int(a)
     else:
         raise TypeError("a must be an integer")
 
-    if type(b) is int or type(b) is float:
+    if type(b) is int or type(b) is float or b is Nan:
         b = int(b)
     else:
         raise TypeError("b must be an integer")
