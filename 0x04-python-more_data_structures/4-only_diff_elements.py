cdf6b5406c713d96b3b109c9605e49beab7b393e
Alouie412 <alouie412@gmail.com>
2020-02-02 09:53:23 +0000

    Logic rewrite

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    common_list = []
-    for i in set_2:
-        if i not in set_i:
-            common_list.append(i)
-    return common_list
+    a = set(set_1)
+    b = set(set_2)
+    return a ^ b
