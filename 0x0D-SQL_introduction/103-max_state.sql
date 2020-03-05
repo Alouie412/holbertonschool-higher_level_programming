3a3b95d14f512cd0583397fb7077f7743b18516b
Alouie412 <alouie412@gmail.com>
2020-03-05 06:04:55 +0000

    Corrected logic: take max of temperatures.value

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,6 +1,6 @@
 -- This script prints out the highest recorded temperature for each state in descending order
 
-SELECT state, MAX('value') as max_temp
+SELECT state, MAX(temperatures.value) as max_temp
 FROM temperatures
 GROUP BY state
-ORDER BY state DESC;
+ORDER BY state ASC;
