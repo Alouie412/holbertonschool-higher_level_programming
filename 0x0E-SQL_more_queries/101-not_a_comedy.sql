6110e40bb03885ae2a62dccee934d69bcfb1c510
Alouie412 <alouie412@gmail.com>
2020-03-05 04:46:26 +0000

    Initial commit: no comedy shows

---
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -0,0 +1,13 @@
+-- This script displays all shows whose genre are not considered Comedy.
+-- This is the exact opposite of 15-comedy_only.sql, and is similar to 100-not_my_genres.sql
+
+SELECT tv_shows.title
+FROM tv_shows
+WHERE NOT tv_shows.id IN
+    (SELECT tv_shows.id
+    from tv_shows
+    LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+    LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+    WHERE tv_genres.name = 'Comedy'
+)
+ORDER by tv_shows.title ASC;
