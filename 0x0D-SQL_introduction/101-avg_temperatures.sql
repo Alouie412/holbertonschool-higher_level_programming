c97cddab1350c6783d4d417f14abfb6bd79f1943
Alouie412 <alouie412@gmail.com>
2020-03-05 05:59:54 +0000

    Corrected logic: get avg of temperature.value

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,5 +1,6 @@
 -- This script takes the average temperature of all cities, then lists them in descending order
 
-SELECT city, AVG('value') as avg_temp
+SELECT city, AVG(temperatures.value) as avg_temp
 FROM temperatures
+GROUP BY city
 ORDER BY avg_temp DESC;
