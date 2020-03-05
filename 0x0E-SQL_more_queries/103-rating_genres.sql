faac962f526f5cd4e638eb97650e478682a788e8
Alouie412 <alouie412@gmail.com>
2020-03-05 05:24:25 +0000

    Initial commit: even more joins

---
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -0,0 +1,10 @@
+-- This script displays the genres and total ratings, grouped by genre name
+-- This requires all the tables within hbtn_0d_tvshows_rate, thus multiple joins
+
+SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS rating
+FROM tv_genres
+INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+INNER JOIN tv_show_ratings ON tv_show_ratings.show_id = tv_shows.id
+GROUP BY tv_genres.name
+ORDER BY rating DESC;
