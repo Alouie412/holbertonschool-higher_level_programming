28aa2192f1a556d2e4b9839e4b9d69b5709075da
Alouie412 <alouie412@gmail.com>
2020-04-13 04:51:57 +0000

    Incorrect tag used with curl. Should be -s, not -S

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script sends a POST request to the given URL and returns the result
-curl -S "$1" -d "email=hr@holbertonschool.com&subject=I will always be here for PLD"
+curl -s "$1" -d "email=hr@holbertonschool.com&subject=I will always be here for PLD"
