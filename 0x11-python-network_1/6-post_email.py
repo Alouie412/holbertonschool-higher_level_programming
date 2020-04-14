0677951b617cde725c97972e3f0b88c097f0a3b1
Alouie412 <alouie412@gmail.com>
2020-04-14 05:56:47 +0000

    Initial commit: POST request, requests ver

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Send a POST request using passed in email and display the
+body of the response. Requests version
+"""
+import requests
+import sys
+
+
+def post_email():
+    new_dict = {'email': sys.argv[2]}
+    req = requests.post(argv[1], new_dict)
+    print(req.text)
+
+
+if __name__ == '__main__':
+    post_email()
