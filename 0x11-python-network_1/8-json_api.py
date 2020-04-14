63e45e9cdb1a9252d1e7e5c9121f746ee5b98c7a
Alouie412 <alouie412@gmail.com>
2020-04-14 06:11:11 +0000

    Initial commit: search an API

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Send a post request to a specific url and display the results depending
+on the passed in parameter and if the response is JSON formatted
+"""
+import requests
+import sys
+
+
+def search_api():
+    if len(argv) == 1:
+        q = ""
+    else
+        q = sys.argv[1]
+
+    new_dict = {'q': q}
+    req = requests.post("http://0.0.0.0:5000/search_user", q)
+
+    try:
+        user = req.json()
+        if user is not None:
+            print("[{}] {}".format(user.get('id'), user.get('name')))
+        else:
+            print("No result")
+    except:
+        print("Not a valid JSON")
+
+
+if __name__ == '__main__':
+    search_api()
