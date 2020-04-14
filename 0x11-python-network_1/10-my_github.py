11cdce5552add547c608bbfc206c045e945e0c32
Alouie412 <alouie412@gmail.com>
2020-04-14 06:24:18 +0000

    Initial commit: Github API

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Display the id of a Github user using Github's API """
+import requests
+import sys
+
+
+def get_hub():
+    req = requests.get("https://api.github.com/user", auth=(sys.argv[1], sys.argv[2]))
+    print(req.json().get('id'))
+
+
+if __name__ == '__main__':
+    get_hub()
