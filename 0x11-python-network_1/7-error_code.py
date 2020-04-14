947f21ba6716dcfdb51d9ce2aa93d9b1c7d9e77a
Alouie412 <alouie412@gmail.com>
2020-04-14 06:01:32 +0000

    Initial commit: status code, requests ver

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Send a request to the passed in url and display the body of the response
+If it fails for whatever reason, display the status code. Requests version
+"""
+import requests
+import sys
+
+def error():
+    req = requests.get(sys.argv[1])
+    if r.status_code >= 400:
+        print("Error code: {}".format(req.status_code))
+    else:
+        print(req.text)
+
+
+if __name__ == '__main__':
+    error()
