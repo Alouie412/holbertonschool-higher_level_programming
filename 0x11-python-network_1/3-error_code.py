667e346363ca41f078da4c6ff03b7eb3cc46f4f8
Alouie412 <alouie412@gmail.com>
2020-04-14 05:18:48 +0000

    Initial commit: status code

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Send a request to the passed in url and display the body of the response
+If it fails for whatever reason, display the status code
+"""
+import urllib.request
+import sys
+
+
+def error():
+    try:
+        req = urllib.request.Request(sys.argv[1], data)
+        with urllib.request.urlopen(req) as msg:
+            print(msg.read().decode('utf-8'))
+    except urllib.error.HTTPError as err:
+        #print("Error code: {}".format(err.read().decode('utf-8')))
+        print("Error code: {}".format(err.getcode()))
+
+
+if __name__ == '__main__':
+    error()
