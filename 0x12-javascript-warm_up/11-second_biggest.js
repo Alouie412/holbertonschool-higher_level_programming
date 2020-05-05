32d4c9737d7c84ef5a34aff8a43a0d89181ccb7e
Alouie412 <alouie412@gmail.com>
2020-05-05 22:42:07 +0000

    Fixed logic: purge array of all NaNs before popping off largest value

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -7,10 +7,24 @@ let i;
 if (length === 2 || length === 3) {
   console.log(0);
 } else {
-  for (i = 2; i < length; i++) { array.push(parseInt(process.argv[i])); }
+  for (i = 2; i < length; i++) {
+    array.push(parseInt(process.argv[i]));
+  }
 
   array.sort();
-  array.pop();
+  console.log(array);
+  console.log(i);
+  /*
+    Because of how sorting works, NaN always comes after
+    numbers. This for loop is to purge the array of all NaNs
+    Also length - 3. 1 because of how arrays are counted, and 2
+    because of the execution of this file
+  */
+  for (i = length - 3; isNaN(array[i]); i--)
+    array.pop();
 
+  // After purging all NaNs (if any), get rid of the largest value
+  // because we want the 2nd largest value in the array
+  array.pop();
   console.log(Math.max.apply(null, array));
 }
