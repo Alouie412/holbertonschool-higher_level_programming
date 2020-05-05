29a85aaa1e8503ade9b306417330a0db9e9860c6
Alouie412 <alouie412@gmail.com>
2020-05-05 22:59:19 +0000

    Logic rewrite: only allow valid integers into the array

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,28 +2,30 @@
 
 const length = process.argv.length;
 const array = [];
+let value;
 let i;
 
 if (length === 2 || length === 3) {
   console.log(0);
 } else {
   for (i = 2; i < length; i++) {
-    array.push(parseInt(process.argv[i]));
+    value = parseInt(process.argv[i]);
+
+    /*
+      There were some issues using the sort function with NaN
+      in the array. As a workaround, only push to array if
+      value contains a valid integer
+    */
+    if (!isNaN(value)) { array.push(value); }
   }
 
-  array.sort();
   /*
-    Because of how sorting works, NaN always comes after
-    numbers. This for loop is to purge the array of all NaNs
-    Also length - 3. 1 because of how arrays are counted, and 2
-    because of the execution of this file
+    Since we want the 2nd largest value in the list of arguments,
+    we can easily get that by sorting the array from low to high, then
+    pop off the very last node. Note that sort by default sorts alphabetically,
+    which causes problems, so a dummy function is passed into sort
   */
-  for (i = length - 3; isNaN(array[i]); i--) {
-    array.pop();
-  }
-
-  // After purging all NaNs (if any), get rid of the largest value
-  // because we want the 2nd largest value in the array
+  array.sort(function (a, b) { return a - b; });
   array.pop();
   console.log(Math.max.apply(null, array));
 }
